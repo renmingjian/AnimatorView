1169be7b54a3e9f693febf484f9b18c327fd27ee
renmj <renmj>
2020-10-09 15:45:13 +0800

    柱状图数据修改

--- app/src/main/java/com/erge/animatorview/activity/HistogramActivity.kt
+++ app/src/main/java/com/erge/animatorview/activity/HistogramActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.erge.animatorview.R
 import com.erge.animatorview.view.HistogramView
-import java.util.ArrayList
+import java.util.*
 
 class HistogramActivity : AppCompatActivity() {
 
@@ -19,9 +19,12 @@ class HistogramActivity : AppCompatActivity() {
         histogramView = findViewById(R.id.histogramView)
 
 
+        val random = Random()
         for (i in 0..19) {
             val info = HistogramView.HistogramInfo()
-            info.count = 10 + i
+            val count = random.nextInt(31)
+            info.count = count
+//            info.count = 10 + i
             info.time = if (i >= 10) "07-" + (i + 1) else "07-0$i"
 
             data.add(info)
