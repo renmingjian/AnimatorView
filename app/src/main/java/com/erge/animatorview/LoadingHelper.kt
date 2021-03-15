e03bcedcff6e4522c5f9af3d1ab7e071bff38314
renmingjian <renmingjian@shouqiev.com>
2021-03-15 23:34:06 +0800

    MotionLayout动画代码抽取

--- app/src/main/java/com/erge/animatorview/LoadingHelper.kt
+++ app/src/main/java/com/erge/animatorview/LoadingHelper.kt
@@ -1,40 +1,68 @@
 package com.erge.animatorview
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.app.Activity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.Window
+import android.widget.Toast
 import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.constraintlayout.motion.widget.TransitionAdapter
 import androidx.constraintlayout.widget.ConstraintSet
+import com.airbnb.lottie.LottieAnimationView
 
-class LoadingHelper(private val activity: Activity, private val view: View) {
+class LoadingHelper(activity: Activity, private val targetEndView: View) {
 
-    private lateinit var root: MotionLayout
-    private lateinit var v_test: View
-    val array = IntArray(2)
-    private lateinit var constraintSet: ConstraintSet
+    private var root: MotionLayout
+    private var targetStartView: View
+    private val array = IntArray(2)
     private var contentParent: ViewGroup? = null
+    private var lottieView: LottieAnimationView
 
     init {
-        contentParent = activity.window.decorView.findViewById<ViewGroup>(Window.ID_ANDROID_CONTENT)
+        contentParent = activity.window.decorView.findViewById(Window.ID_ANDROID_CONTENT)
         val loadingLayout = LayoutInflater.from(activity)
-            .inflate(R.layout.lottie_loading, contentParent, true) as ViewGroup
-        root = loadingLayout.findViewById(R.id.root)
-        v_test = loadingLayout.findViewById(R.id.v_test)
-        constraintSet = root.getConstraintSet(R.xml.scene1)
+            .inflate(R.layout.lottie_loading, contentParent, false) as ViewGroup
+        contentParent!!.addView(loadingLayout)
+        root = contentParent!!.findViewById(R.id.root)
+        targetStartView = contentParent!!.findViewById(R.id.view)
+        lottieView = contentParent!!.findViewById(R.id.lottieView)
+
+        lottieView.addAnimatorListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator?) {
+                super.onAnimationEnd(animation)
+                // 给view设置最后一帧时图片的大小
+                targetStartView.visibility = View.VISIBLE
+                lottieView.visibility = View.INVISIBLE
+
+
+                targetStartView.performClick()
+                Toast.makeText(activity, "onAnimationEnd", Toast.LENGTH_SHORT).show()
+
+            }
+        })
+
+        root.addTransitionListener(object : TransitionAdapter() {
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+                super.onTransitionCompleted(motionLayout, currentId)
+                lottieView.visibility = View.GONE
+                contentParent!!.removeView(loadingLayout)
+            }
+        })
 
         // loading过程
-        view.postDelayed({
+        targetStartView.postDelayed({
             start()
         }, 300)
     }
 
     fun start() {
+        val constraintSet = root.getConstraintSet(R.xml.scene1)
         val contentArray = IntArray(2)
         contentParent!!.getLocationInWindow(contentArray)
-        println("barHeight = ${contentArray[1]}")
-        v_test.getLocationInWindow(array)
+        targetEndView.getLocationInWindow(array)
         constraintSet.connect(
             R.id.view,
             ConstraintSet.TOP,
@@ -49,9 +77,7 @@ class LoadingHelper(private val activity: Activity, private val view: View) {
             ConstraintSet.START,
             array[0]
         )
-        println("xxx = ${array[0]}")
-
-        view.performClick()
+        constraintSet.applyTo(root)
     }
 
 }
--- app/src/main/java/com/erge/animatorview/activity/MotionLayoutActivity.kt
+++ app/src/main/java/com/erge/animatorview/activity/MotionLayoutActivity.kt
@@ -1,48 +1,30 @@
 package com.erge.animatorview.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.view.ViewGroup
-import android.view.Window
-import androidx.constraintlayout.motion.widget.MotionLayout
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.constraintlayout.widget.ConstraintSet
+import androidx.appcompat.app.AppCompatActivity
+import com.erge.animatorview.LoadingHelper
 import com.erge.animatorview.R
-import com.erge.animatorview.utils.Utils
 
 class MotionLayoutActivity : AppCompatActivity() {
 
-    private lateinit var root: MotionLayout
     private lateinit var v_test: View
-    val array = IntArray(2)
-    private lateinit var constraintSet1: ConstraintSet
+    private lateinit var loadingHelper: LoadingHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_motion_layout)
 
-        val view: View = findViewById(R.id.view)
         v_test = findViewById(R.id.v_test)
 
+        loadingHelper = LoadingHelper(this, v_test)
 
-        root = findViewById(R.id.root)
-        constraintSet1 = root.getConstraintSet(R.xml.scene1)
 
-        view.postDelayed({
-            view.performClick()
-        }, 1000)
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
-        val contentParent = window.decorView.findViewById<ViewGroup>(Window.ID_ANDROID_CONTENT)
-        val contentArray = IntArray(2)
-        contentParent.getLocationInWindow(contentArray)
-        println("barHeight = ${contentArray[1]}")
-        v_test.getLocationInWindow(array)
-        constraintSet1.connect(R.id.view, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, array[1] - contentArray[1])
-        constraintSet1.connect(R.id.view, ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START, array[0])
-        println("xxx = ${array[0]}")
+        loadingHelper.start()
     }
+
 }
